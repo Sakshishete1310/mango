20648b1ce30c60870cb245ee62c80bac67189b47
SAKSHIshete@123 <sakshishete1310@gmail.com>
2023-02-23 22:10:46 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import'./App.css';
+import Card from './Card';
+function App(){
+    return(
+    <>
+    <div className="container">
+    <h1>Hallo world</h1> 
+    <Card/>   <Card/>  <Card/> <Card/> 
+    </div>
+    </>
+    );
+    }
+    export default App;
---
+++ src/Card.js
@@ -0,0 +1,9 @@
+import'./Card.css';
+function Card(){
+    return(
+        <div className="Card">
+            <h2>This is a Card</h2>
+        </div>
+    );
+}
+export default Card;
